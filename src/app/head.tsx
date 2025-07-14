9e179654fdb4610745083703699e289a4e496eb0
Bersek <jeffrygarzon1@gmail.com>
2025-07-14 03:53:02 -0500

    Actualización sniper

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -7,14 +7,14 @@ export default function Head() {
       {/* OG para compartir en Facebook, WhatsApp, etc */}
       <meta property="og:title" content="Core Sync Collective - Fiesta Techno" />
       <meta property="og:description" content="No te pierdas el evento del año: 16 de agosto, Mariquita. Boletas limitadas. ¡Compra ahora!" />
-      <meta property="og:image" content="https://localhost:3000/evento-foto.jpg" />
-      <meta property="og:url" content="https://localhost:3000" />
+      <meta property="og:image" content="https://core-sync-4voc.vercel.app//evento-foto.jpg" />
+      <meta property="og:url" content="https://core-sync-4voc.vercel.app/" />
       <meta property="og:type" content="website" />
       {/* Twitter Card */}
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:title" content="Core Sync Collective - Fiesta Techno" />
       <meta name="twitter:description" content="No te pierdas el evento del año: 16 de agosto, Mariquita. Boletas limitadas. ¡Compra ahora!" />
-      <meta name="twitter:image" content="https://localhost:3000/evento-foto.jpg" />
+      <meta name="twitter:image" content="https://core-sync-4voc.vercel.app//evento-foto.jpg" />
       {/* Favicon */}
       <link rel="icon" href="/favicon.ico" />
     </>
