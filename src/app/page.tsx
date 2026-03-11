4a2d869c06bcfac59331493797c3fc030e96e39f
Bersek <jeffrygarzon1@gmail.com>
2026-03-11 17:36:51 -0500

    x

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -407,7 +407,7 @@ export default function Home() {
               <div className="px-8 pt-2 pb-10 max-h-[65vh] overflow-y-auto scrollbar-thin scrollbar-thumb-gray-300 scrollbar-track-gray-100">
                 <div className="w-full flex justify-center mb-4">
                   <Image
-                    src="/evento-foto-velitas.png"
+                    src="/evento-samana-santa.png"
                     alt="Flyer Core Sync Collective"
                     className="rounded-xl max-h-80 md:max-h-96 w-auto object-contain shadow-lg"
                   />
